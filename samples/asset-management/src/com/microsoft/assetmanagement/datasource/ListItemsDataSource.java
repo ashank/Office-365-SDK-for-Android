e77c4f4de607efa503929f97d33eab3c29f0b41e
Marcos Torres <marcost@lagash.com>
2014-02-28 16:30:45 -0300

    adding comments and javadocs for asset management sample app

--- samples/asset-management/src/com/microsoft/assetmanagement/datasource/ListItemsDataSource.java
+++ samples/asset-management/src/com/microsoft/assetmanagement/datasource/ListItemsDataSource.java
@@ -33,6 +33,12 @@ public class ListItemsDataSource {
 		return mApplication.getCurrentListClient();
 	}
 
+	/**
+	 * Returns a ArrayList<CarListViewItem> with the list item information of a car
+	 * 
+	 * @return ArrayList<CarListViewItem>
+	 * @throws Exception
+	 */
 	public ArrayList<CarListViewItem> getDefaultListViewItems() throws Exception {
 		final ArrayList<CarListViewItem> items = new ArrayList<CarListViewItem>();
 
@@ -40,22 +46,32 @@ public class ListItemsDataSource {
 		final String listName = mApplication.getPreferences().getLibraryName();
 		int topCount = mApplication.getPreferences().getListDisplaySize();
 		
+		//Sharepoint list columns we want to query
 		String[] columns = mApplication.getApplicationContext().getResources()
 				.getStringArray(R.array.visibleListColumns);
+		
+		//Get the list of items from a given sharepoint list name. 
+		//We do a projection (select) and top (OData operators) in order to retrieve the lists.
+		//We call get(), a blocking operation but since this call is being called 
+		//from an async task we are not freezing the UI thread
 		List<SPListItem> listItems = client.getListItems(listName,
 				new Query().select(columns).top(topCount)).get();
 
 		List<OfficeFuture<DocumentLibraryItem>> futures = new ArrayList<OfficeFuture<DocumentLibraryItem>>();
 		for (final SPListItem carItem : listItems) {
+			//Here we get the picture data.
 			int pictureId = Integer.parseInt(carItem.getData("ID").toString());
 			futures.add(client.getFileFromDocumentLibrary(listName, String.valueOf(pictureId), true));
 		}
 
+		//Here we wait for completion of all futures.
 		List<DocumentLibraryItem> pictures = OfficeFuture.all(futures).get();
 
 		for (final SPListItem carItem : listItems) {
 			for (DocumentLibraryItem picture : pictures) {
 				if (String.valueOf(carItem.getId()).equals(picture.getItemId())) {
+					//Here we bind together the car information with it's picture 
+					//This is the information used by the adapter to bind the data to the list view.
 					items.add(new CarListViewItem(carItem, picture.getContent()));
 					break;
 				}
@@ -82,8 +98,10 @@ public class ListItemsDataSource {
 		SharepointListsClientWithFiles client = (SharepointListsClientWithFiles) getListsClient();
 		String listName = mApplication.getPreferences().getLibraryName();
 		if (client != null) {
+			//We call a picture from a picture library with a given list name and the item id
 			SPFile spFile = client.getSPFileFromPictureLibrary(listName, carViewItem.getCarId())
 					.get();
+			//upload the file to the sharepoint list
 			client.uploadFile(listName, spFile.getData("Name").toString(), carViewItem.getPicture())
 					.get();
 		}
@@ -93,9 +111,12 @@ public class ListItemsDataSource {
 		SharepointListsClientWithFiles client = (SharepointListsClientWithFiles) getListsClient();
 		String listName = mApplication.getPreferences().getLibraryName();
 
+		//upload a new file with a randome name
 		SPFile file = client.uploadFile(listName, UUID.randomUUID().toString() + ".png",
 				carViewItem.getPicture()).get();
+		//retrieves the picture url from the file metadata.
 		String pictureUrl = file.getData("ServerRelativeUrl").toString();
+		//we get the actual id from an url
 		String id = client.getListItemIdForFileByServerRelativeUrl(pictureUrl).get();
 		return Integer.parseInt(id);
 	}
@@ -103,8 +124,10 @@ public class ListItemsDataSource {
 	private void updateCarData(CarListViewItem carViewItem) throws Exception {
 		SharepointListsClient client = getListsClient();
 		String listName = mApplication.getPreferences().getLibraryName();
+		//get the car list
 		SPList carList = client.getList(listName).get();
 		SPListItem item = carViewItem.getListItem();
+		//updates the list item from the view model
 		client.updateListItem(item, carList).get();
 	}
 
@@ -113,6 +136,7 @@ public class ListItemsDataSource {
 		String listName = mApplication.getPreferences().getLibraryName();
 
 		try {
+			//we delete a given list item from a list.
 			client.deleteListItem(mCarViewItem.getListItem(), listName).get();
 		} catch (Throwable t) {
 			t.printStackTrace();
