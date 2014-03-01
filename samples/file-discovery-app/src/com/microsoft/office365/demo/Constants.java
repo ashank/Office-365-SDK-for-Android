b8b62426cf0be4eb82448c423b5870a8dc59f007
Roopali <roopalik@users.noreply.github.com>
2014-02-28 17:10:55 -0800

    Update Constants.java
    Added a note for resource ID - might change in the future.

--- samples/file-discovery-app/src/com/microsoft/office365/demo/Constants.java
+++ samples/file-discovery-app/src/com/microsoft/office365/demo/Constants.java
@@ -5,6 +5,10 @@ public class Constants {
 	public static final String AUTHORITY_URL = "https://login.windows-ppe.net/common/oauth2/token";
 	public static final String CLIENT_ID = "your-client-id"; 
 	public static final String REDIRECT_URL = "http://your-redirect-url.com";
+	
+	// In this initial Preview release, you must use a temporary Resource ID for Service Discovery ("Microsoft.SharePoint").
+        // TODO: If this Resource ID ceases to work, check for an updated value at http://go.microsoft.com/fwlink/?LinkID=392944
+
 	public static final String DISCOVERY_RESOURCE_ID =  "Microsoft.SharePoint";
 	public static final String MYFILES_CAPABILITY = "MyFiles";
 	
